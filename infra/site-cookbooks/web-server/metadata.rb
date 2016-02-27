153db22c695ee0ad95bba7217da27339a4260662
davcdhanesh1 <davcdhanesh1@gmail.com>
2016-02-27 18:06:21 +0530

    Dhanesh - Uses simple epel-release repo to install nodejs

---
+++ infra/site-cookbooks/web-server/metadata.rb
@@ -0,0 +1,7 @@
+name             'web-server'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures web-server'
+long_description 'Installs/Configures web-server'
+version          '0.1.0'
---
+++ infra/site-cookbooks/web-server/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: web-server
+# Recipe:: default
+#
+# Copyright (C) 2016 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+
+yum_package 'epel-release'
+yum_package 'nodejs'
+yum_package 'npm'
